0cc5e7cf8e42c05e4349f20c588f1720fb2c5166
Federico <federico.creti87@gmail.com>
2020-08-17 09:39:54 +0200

    Added client sample project

---
+++ MQTTClientTest/Program.cs
@@ -0,0 +1,119 @@
+﻿using MQTTnet;
+using MQTTnet.Client;
+using MQTTnet.Client.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MQTTClientTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            while (true)
+            {
+                System.Threading.Thread.Sleep(10);
+                var operation = Console.ReadKey();
+                if (operation.Key == ConsoleKey.D1)
+                {
+                    connectAndRegisterToTopic();
+                }
+                else if (operation.Key == ConsoleKey.D2)
+                {
+                    Console.WriteLine("Type the message to send and click enter");
+                    string message = Console.ReadLine();
+                    connectAndSendToTopic(message);
+                }
+            }
+        }
+
+        static async void connectAndSendToTopic(string messageToSend)
+        {
+            var factory = new MqttFactory();
+            var mqttClient = factory.CreateMqttClient();
+            var options = new MqttClientOptionsBuilder().WithTcpServer("127.0.0.1").Build();
+            try
+            {
+                var conn = await mqttClient.ConnectAsync(options, CancellationToken.None); // Since 3.0.5 with CancellationToken             
+
+                //mqttClient.UseApplicationMessageReceivedHandler(e =>
+                //{
+                //    Console.WriteLine("### RECEIVED APPLICATION MESSAGE ###");
+                //    Console.WriteLine($"+ Topic = {e.ApplicationMessage.Topic}");
+                //    Console.WriteLine($"+ Payload = {Encoding.UTF8.GetString(e.ApplicationMessage.Payload)}");
+                //    Console.WriteLine($"+ QoS = {e.ApplicationMessage.QualityOfServiceLevel}");
+                //    Console.WriteLine($"+ Retain = {e.ApplicationMessage.Retain}");
+                //    Console.WriteLine();
+
+                //    //Task.Run(() => mqttClient.PublishAsync("my/topic"));
+                //});
+
+                var message = new MqttApplicationMessageBuilder()
+                    .WithTopic("my/topicc")
+                    .WithPayload(messageToSend)
+                    .WithExactlyOnceQoS()
+                    .Build();
+
+                var res = await mqttClient.PublishAsync(message, CancellationToken.None); // Since 3.0.5 with CancellationToken
+            }
+            catch (Exception ex)
+            {
+
+            }
+
+            Console.WriteLine("Connected");
+        }
+
+        static async void connectAndRegisterToTopic()
+        {
+           
+            var factory = new MqttFactory();
+            var mqttClient = factory.CreateMqttClient();
+            var options = new MqttClientOptionsBuilder().WithTcpServer("127.0.0.1").Build();
+            try
+            {
+                mqttClient.UseConnectedHandler(async e =>
+                {
+                    Console.WriteLine("### CONNECTED WITH SERVER ###");
+
+                    // Subscribe to a topic
+                    await mqttClient.SubscribeAsync(new MqttTopicFilterBuilder().WithTopic("my/topicc").Build());
+
+                    Console.WriteLine("### SUBSCRIBED ###");
+                });
+
+                var conn = await mqttClient.ConnectAsync(options, CancellationToken.None); // Since 3.0.5 with CancellationToken             
+
+                mqttClient.UseApplicationMessageReceivedHandler(e =>
+                {
+                    Console.WriteLine("### RECEIVED APPLICATION MESSAGE ###");
+                    Console.WriteLine($"+ Topic = {e.ApplicationMessage.Topic}");
+                    Console.WriteLine($"+ Payload = {Encoding.UTF8.GetString(e.ApplicationMessage.Payload)}");
+                    Console.WriteLine($"+ QoS = {e.ApplicationMessage.QualityOfServiceLevel}");
+                    Console.WriteLine($"+ Retain = {e.ApplicationMessage.Retain}");
+                    Console.WriteLine();
+
+                    //Task.Run(() => mqttClient.PublishAsync("my/topic"));
+                });
+
+                /*var message = new MqttApplicationMessageBuilder()
+                    .WithTopic("my/topicc")
+                    .WithPayload("Hello World!!!!!!!!!!")
+                    .WithExactlyOnceQoS()
+                    .Build();
+
+                var res = await mqttClient.PublishAsync(message, CancellationToken.None);*/ // Since 3.0.5 with CancellationToken
+            } catch(Exception ex)
+            {
+
+            }
+            
+            Console.WriteLine("Connected");
+        }
+    }
+}
---
+++ MQTTClientTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("MQTTClientTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MQTTClientTest")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("5c58b355-15f5-4a07-a7a6-80d1d61648c8")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
